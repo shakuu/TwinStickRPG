ddc38426fff7569a86547ed28f29994379d61687
shakuu <todorov.h@gmail.com>
2016-08-22 23:24:44 +0300

    IdProvider tests

--- scripts/controls/controls-manager.js
+++ scripts/controls/controls-manager.js
@@ -15,6 +15,32 @@
 
         };
 
+        Constructor.prototype.addDefinition = function (definition) {
+            if (!definition || !definition.name || !definition.keyCode) {
+                throw new Error('invalid definition');
+            }
+
+            controlDefinitionsByControlName[definition.name] = definition;
+            controlDefinitionsByKeyCode[definition.keyCode] = definition;
+        };
+
+        Constructor.prototype.removeDefinition - function (definition) {
+            controlDefinitionsByControlName[definition.name] = null;
+            controlDefinitionsByKeyCode[definition.keyCode] = null;
+        };
+
+        Constructor.prototype.getControlState = function (controlName) {
+            if (typeof controlName !== 'string') {
+                throw new Error('controlName parameter must be a string.');
+            }
+
+            if (!controlDefinitionsByControlName[controlName]) {
+                throw new Error(controlName + ' is not defined.');
+            }
+
+            return controlDefinitionsByControlName[controlName];
+        };
+
         return Constructor;
     } ());
 
